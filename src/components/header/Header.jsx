35b28c85acfe309067fa2d4313f9e6753556c8a4
delschadj <delschad.jankir@gmx.de>
2022-05-14 10:15:55 +0200

    Header Component JSX

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import "./header.css"
+import CTA from "./CTA"
+import ME from "../../assets/me.png"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I´m</h5>
+        <h1>Delschad</h1>
+        <h5 className='text-light'>Full Stack Developer</h5>
+        <CTA></CTA>
+
+        <div className="me">
+          <img src={ME} alt="" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>
+          Scroll Down
+        </a>
+      </div>
+    </header>
   )
 }
 
